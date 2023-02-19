422e9bf99babe0eb2c7298ce0cdcf4f67e2a3ad8
gdm <gdmichelis@gmail.com>
2023-02-19 23:33:36 +0200

    First commit 1.0.0

--- filter.js
+++ filter.js
@@ -5,14 +5,18 @@ const shoeSex = document.querySelector('#shoe-sex');
 const shoePrice = document.querySelector('#shoe-price');
 const getTableRows = document.getElementsByTagName('tr');
 const filterReset = document.querySelector('#filter-reset');
-let searchFilter = document.querySelector('#search');
+const searchFilter = document.querySelector('#search');
 let shoePriceRange = [];
 let filtersTrue = [];
 let filters = {};
-let x;
+let shoePriceNoCurrency;
 
-function filtersHelper(a) {
-    a.preventDefault();
+/**
+ * Filter Function
+ * @param {object} e 
+ */
+function filter(e) {
+    e.preventDefault();
     for( let i = 0; i < 4; i++) {
        if(shoeSize.value != "") {
         filters.size = shoeSize.value;
@@ -38,17 +42,6 @@ function filtersHelper(a) {
         filters.price = "";
        }
     }
-}
-
-
-//Multiple Select Filters on Shoes Table
-
-/**
- * Shoe Size Filter
- */
-shoeSize.addEventListener( 'change' , (e) => {
-    
-    filtersHelper(e);
     filtersTrue = Object.entries(filters);
     for(let i = 1; i < getTableRows.length; i++ ) {
         if(getTableRows[i].children[3].innerText.includes(filtersTrue[0][1]) == true) {
@@ -89,199 +82,49 @@ shoeSize.addEventListener( 'change' , (e) => {
     if(filtersTrue[3][1] != "") {
         for(let i = 1; i < getTableRows.length; i++ ) {
              shoePriceRange = filtersTrue[3][1].split('-');
-             x = getTableRows[i].children[6].innerText.replace('€', '');
+             shoePriceNoCurrency = getTableRows[i].children[6].innerText.replace('€', '');
              if(getTableRows[i].classList.contains("d-table-row")){
-                 if(x <= Number(shoePriceRange[0]) || x >= Number(shoePriceRange[1])) {
+                 if(shoePriceNoCurrency <= Number(shoePriceRange[0]) || shoePriceNoCurrency >= Number(shoePriceRange[1])) {
                       getTableRows[i].classList.remove("d-table-row");
                       getTableRows[i].classList.add("d-none");
                  }
              }
         }
     }
-    
-});
+}
 
+//Multiple Select Filters on Shoes Table
+
+/**
+ * Shoe Size Filter
+ */
+shoeSize.addEventListener( 'change' , filter);
 
 
 /**
  * Shoe Color Filter
  */
-shoeColor.addEventListener( 'change' , (e) => {
-    filtersHelper(e);
-    filtersTrue = Object.entries(filters);
-    for(let i = 1; i < getTableRows.length; i++ ) {
-        if(getTableRows[i].children[4].innerText.includes(filtersTrue[1][1]) == true) {
-            getTableRows[i].classList.add('d-table-row');
-            getTableRows[i].classList.remove('d-none');
-
-        } else {
-            getTableRows[i].classList.add('d-none');
-            getTableRows[i].classList.remove('d-table-row');
-        }
-    }
-    
-    if(filtersTrue[0][1] != "") {
-        for(let i = 1; i < getTableRows.length; i++ ) {
-                 if(getTableRows[i].classList.contains("d-table-row")) {
-                    if(getTableRows[i].children[3].innerText.includes(filtersTrue[0][1]) != true){
-                            getTableRows[i].classList.remove("d-table-row");
-                            getTableRows[i].classList.add("d-none");
-                    }
-                 }
-        }
-    } 
-
-    
-    if(filtersTrue[2][1] != "") {
-        for(let i = 1; i < getTableRows.length; i++ ) {
-            if(getTableRows[i].classList.contains("d-table-row")){
-               if(getTableRows[i].children[5].innerText != filtersTrue[2][1]){
-                   getTableRows[i].classList.remove("d-table-row");
-                   getTableRows[i].classList.add("d-none");
-                }    
-                      
-            }  
-        }
-    } 
-    
-    
-    if(filtersTrue[3][1] != "") {
-        for(let i = 1; i < getTableRows.length; i++ ) {
-             shoePriceRange = filtersTrue[3][1].split('-');
-             x = getTableRows[i].children[6].innerText.replace('€', '');
-             if(getTableRows[i].classList.contains("d-table-row")){
-                 if(x <= Number(shoePriceRange[0]) || x >= Number(shoePriceRange[1])) {
-                      getTableRows[i].classList.remove("d-table-row");
-                      getTableRows[i].classList.add("d-none");
-                 }
-             }
-        }
-    }
-    
-});
+shoeColor.addEventListener( 'change' , filter);
 
  
-
 /**
  * Shoe Sex Filter
  */
-shoeSex.addEventListener( 'change' , (e) => {
-    filtersHelper(e);
-    filtersTrue = Object.entries(filters);
-    for(let i = 1; i < getTableRows.length; i++ ) {
-        if(getTableRows[i].children[5].innerText == filtersTrue[2][1]) {
-            getTableRows[i].classList.add('d-table-row');
-            getTableRows[i].classList.remove('d-none');
-
-        } else {
-            getTableRows[i].classList.add('d-none');
-            getTableRows[i].classList.remove('d-table-row');
-        }
-    }
-    
-    if(filtersTrue[1][1] != "") {
-        for(let i = 1; i < getTableRows.length; i++ ) {
-                 if(getTableRows[i].classList.contains("d-table-row")) {
-                    if(getTableRows[i].children[4].innerText.includes(filtersTrue[1][1]) != true){
-                            getTableRows[i].classList.remove("d-table-row");
-                            getTableRows[i].classList.add("d-none");
-                    }
-                 }
-        }
-    } 
-
-    
-    if(filtersTrue[0][1] != "") {
-        for(let i = 1; i < getTableRows.length; i++ ) {
-            if(getTableRows[i].classList.contains("d-table-row")){
-               if(getTableRows[i].children[3].innerText.includes(filtersTrue[0][1]) != true ){
-                   getTableRows[i].classList.remove("d-table-row");
-                   getTableRows[i].classList.add("d-none");
-                }    
-                      
-            }  
-        }
-    } 
-    
-    
-    if(filtersTrue[3][1] != "") {
-        for(let i = 1; i < getTableRows.length; i++ ) {
-             shoePriceRange = filtersTrue[3][1].split('-');
-             x = getTableRows[i].children[6].innerText.replace('€', '');
-             if(getTableRows[i].classList.contains("d-table-row")){
-                 if(x <= Number(shoePriceRange[0]) || x >= Number(shoePriceRange[1])) {
-                      getTableRows[i].classList.remove("d-table-row");
-                      getTableRows[i].classList.add("d-none");
-                 }
-             }
-        }
-    }
-    
-});
+shoeSex.addEventListener( 'change' , filter);
   
 
 /**
  * Shoe Price Filter
  */
-shoePrice.addEventListener( 'change' , (e) => {
-    filtersHelper(e);
-    filtersTrue = Object.entries(filters);
-    for(let i = 1; i < getTableRows.length; i++ ) {
-        shoePriceRange = filtersTrue[3][1].split('-');
-        x = getTableRows[i].children[6].innerText.replace('€', '');
-        if(x >= Number(shoePriceRange[0]) && x <= Number(shoePriceRange[1])) {
-            getTableRows[i].classList.add('d-table-row');
-            getTableRows[i].classList.remove('d-none');
+shoePrice.addEventListener( 'change' , filter);
 
-        } else {
-            getTableRows[i].classList.add('d-none');
-            getTableRows[i].classList.remove('d-table-row');
-        }
-    }
-    
-    if(filtersTrue[1][1] != "") {
-        for(let i = 1; i < getTableRows.length; i++ ) {
-                 if(getTableRows[i].classList.contains("d-table-row")) {
-                    if(getTableRows[i].children[4].innerText.includes(filtersTrue[1][1]) != true){
-                            getTableRows[i].classList.remove("d-table-row");
-                            getTableRows[i].classList.add("d-none");
-                    }
-                 }
-        }
-    } 
 
-    
-    if(filtersTrue[2][1] != "") {
-        for(let i = 1; i < getTableRows.length; i++ ) {
-            if(getTableRows[i].classList.contains("d-table-row")){
-               if(getTableRows[i].children[5].innerText != filtersTrue[2][1]){
-                   getTableRows[i].classList.remove("d-table-row");
-                   getTableRows[i].classList.add("d-none");
-                }    
-                      
-            }  
-        }
-    } 
-    
-    
-    if(filtersTrue[0][1] != "") {
-        for(let i = 1; i < getTableRows.length; i++ ) {
-             shoePriceRange = filtersTrue[0][1].split('-');
-             x = getTableRows[i].children[3].innerText.replace('€', '');
-             if(getTableRows[i].classList.contains("d-table-row")){
-                getTableRows[i].children[3].innerText.includes(filtersTrue[3][1]) == true
-                 if(getTableRows[i].children[4].innerText.includes(filtersTrue[0][1]) != true) {
-                      getTableRows[i].classList.remove("d-table-row");
-                      getTableRows[i].classList.add("d-none");
-                 }
-             }
-        }
-    }
-    
-});
-
-// Search Filter
+// Shoe Search Filter
 searchFilter.addEventListener( 'keyup', search);
+
+/**
+ *  Search Function
+ */
 function search() {
     for(let i = 1; i < getTableRows.length; i++ ) {
         if(getTableRows[i].children[2].innerText.toLowerCase().includes(searchFilter.value.toLowerCase()) == true || 
@@ -298,8 +141,13 @@ function search() {
     }
 }
 
-// Reset All Filters
+// Reset All Shoe Filters
 filterReset.addEventListener( 'click', reset);
+
+/**
+ *  Reset Function
+ * @param {object} e 
+ */
 function reset(e) {
    e.preventDefault();
    for(let i = 1; i < getTableRows.length; i++ ) {
